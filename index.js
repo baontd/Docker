ba470cc98e979b76644e6d61cd260ced9c54cab5
BaoNTD <baontdfpt@gmail.com>
2025-07-05 11:02:58 +0700

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, UDTTTTTTTTT!');
+    res.send('Hello, UDTT!');
 });
 
 app.listen(PORT, () => {
