3b3c867262b1b95c85f8036a7e692c12dc1ecaaa
BaoNTD <baontdfpt@gmail.com>
2025-07-05 10:51:42 +0700

    asd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, UDT!');
+    res.send('Hello, asdkjbaknbasd!');
 });
 
 app.listen(PORT, () => {
